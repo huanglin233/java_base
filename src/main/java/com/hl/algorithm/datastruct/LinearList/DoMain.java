60ba5a06b39c0f56cc68c742d105cfd736538d26
huanglin233 <www.2512467648@qq.com>
2024-07-03 23:39:04 +0800

    堆栈相关

--- src/main/java/com/hl/algorithm/datastruct/LinearList/DoMain.java
+++ src/main/java/com/hl/algorithm/datastruct/LinearList/DoMain.java
@@ -2,6 +2,7 @@ package com.hl.algorithm.datastruct.LinearList;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -33,10 +34,57 @@ public class DoMain {
 
         return stack.isEmpty();
     }
+
+
     
     @Test
     public void isValidTest() {
         System.out.println(isValid("()[232]{111}"));
         System.out.println(isValid("(]"));
+    }
+
+    /**
+     * 数组中元素于下一个比他大的元素之间的距离
+     * @param temperatures 数组
+     * @return int[]
+     */
+    public int[] dailyTemperatures(int[] temperatures) {
+        int            num    = temperatures.length;
+        int[]          dist   = new int[num];
+        Stack<Integer> indexs = new Stack<Integer>();
+        for(int curIndex = 0; curIndex < num; curIndex++) {
+            while(!indexs.isEmpty() && temperatures[curIndex] <= temperatures[indexs.peek()]) {
+                int preIndex = indexs.pop();
+                dist[preIndex] = curIndex - preIndex;
+            }
+
+            indexs.add(curIndex);
+        }
+
+        return dist;
+    }
+
+    /**
+     * 循环数组中比当前元素大的下一个元素
+     * @param nums 循环数组
+     * @return
+     */
+    public int[] nextGreaterElements(int[] nums) {
+        int   num  = nums.length;
+        int[] next = new int[num];
+        Arrays.fill(next, -1);
+        Stack<Integer> pre = new Stack<Integer>();
+        for (int i = 0; i < num * 2; i++) {
+            int n = nums[i % num];
+            while(!pre.isEmpty() && nums[pre.peek()] < n) {
+                next[pre.pop()] = n;
+            }
+
+            if(i < num) {
+                pre.push(i);
+            }
+        }
+
+        return next;
     }
 }
